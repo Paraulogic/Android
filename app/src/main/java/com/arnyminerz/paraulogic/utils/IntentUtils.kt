e4ded1b0dea8db73e4a5bc6e72e439d051a0ac8d
Arnau Mora <arnyminer.z@gmail.com>
2022-03-07 22:41:56 +0100

    :sparkles: Added `launch` and `launchUrl`
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

---
+++ app/src/main/java/com/arnyminerz/paraulogic/utils/IntentUtils.kt
@@ -0,0 +1,41 @@
+package com.arnyminerz.paraulogic.utils
+
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+
+/**
+ * Starts the [activity] with the set [parameters].
+ * @author Arnau Mora
+ * @since 20220307
+ * @param activity The class type of the Activity to launch.
+ * @param parameters Used to apply some options to the internal [Intent].
+ */
+fun Context.launch(activity: Class<*>, parameters: Intent.() -> Unit = {}) {
+    startActivity(
+        Intent(this, activity)
+            .apply(parameters)
+    )
+}
+
+/**
+ * Launches the set [uri] as [action] with the set [parameters].
+ * @author Arnau Mora
+ * @since 20220307
+ * @param action The action to run.
+ * @param uri The uri to use.
+ * @param parameters Used to apply some options to the internal [Intent].
+ */
+fun Context.launch(action: String, uri: Uri, parameters: Intent.() -> Unit = {}) =
+    startActivity(
+        Intent(action, uri)
+            .apply(parameters)
+    )
+
+/**
+ * Launches the [url] into the default web browser.
+ * @author Arnau Mora
+ * @since 20220307
+ * @param url The url to launch
+ */
+fun Context.launchUrl(url: String) = launch(Intent.ACTION_VIEW, Uri.parse(url))
