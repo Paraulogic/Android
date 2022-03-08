4ecd3ecc5b2d6dcadf6cbe93f3f4fdc3806dcc26
Arnau Mora <arnyminer.z@gmail.com>
2022-03-08 17:34:48 +0100

    :sparkles: Added help dialog
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- app/src/main/java/com/arnyminerz/paraulogic/MainActivity.kt
+++ app/src/main/java/com/arnyminerz/paraulogic/MainActivity.kt
@@ -8,22 +8,32 @@ import androidx.activity.viewModels
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Info
+import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.material.icons.rounded.Person
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Button
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -50,7 +60,7 @@ class MainActivity : ComponentActivity() {
             val signInResult = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
             if (signInResult?.isSuccess == true) {
                 Timber.i("Signed in correctly.")
-                val signedInAccount = signInResult.signInAccount
+                // val signedInAccount = signInResult.signInAccount
             } else {
                 Timber.e("Sign in status: ${signInResult?.status}")
             }
@@ -68,9 +78,33 @@ class MainActivity : ComponentActivity() {
             val viewModel by viewModels<MainViewModel>()
             val gameInfo by remember { viewModel.loadGameInfo() }
             val foundWords by remember { viewModel.correctWords }
+            var showHelpDialog by remember { mutableStateOf(false) }
 
             AppTheme {
-                // A surface container using the 'background' color from the theme
+                if (showHelpDialog)
+                    AlertDialog(
+                        onDismissRequest = { showHelpDialog = false },
+                        text = {
+                            Column(modifier = Modifier.fillMaxWidth()) {
+                                Text(
+                                    text = stringResource(R.string.info_rules_title),
+                                    style = MaterialTheme.typography.titleMedium,
+                                )
+                                Text(text = stringResource(R.string.info_rules))
+                                Text(
+                                    text = stringResource(R.string.info_points_title),
+                                    style = MaterialTheme.typography.titleMedium,
+                                )
+                                Text(text = stringResource(R.string.info_points))
+                            }
+                        },
+                        confirmButton = {
+                            Button(onClick = { showHelpDialog = false }) {
+                                Text(text = stringResource(R.string.action_close))
+                            }
+                        },
+                    )
+
                 Scaffold(
                     modifier = Modifier.fillMaxSize(),
                     topBar = {
@@ -113,6 +147,27 @@ class MainActivity : ComponentActivity() {
                                         gameInfo!!.maxPoints
                                     )
                             )
+                            Row(
+                                horizontalArrangement = Arrangement.End,
+                                modifier = Modifier.weight(1f)
+                            ) {
+                                IconButton(
+                                    onClick = { showHelpDialog = true }
+                                ) {
+                                    Icon(
+                                        Icons.Outlined.Info,
+                                        stringResource(R.string.image_desc_help)
+                                    )
+                                }
+                                IconButton(
+                                    onClick = { /*TODO*/ }
+                                ) {
+                                    Icon(
+                                        Icons.Outlined.Settings,
+                                        stringResource(R.string.image_desc_settings)
+                                    )
+                                }
+                            }
                         }
                     }
                 ) { paddingValues ->
