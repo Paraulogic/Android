b1e6fc94893041280bd97719161f24ab61388e6e
Arnau Mora <arnyminer.z@gmail.com>
2022-03-08 16:31:42 +0100

    :sparkles: Added `isTuti`
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- app/src/main/java/com/arnyminerz/paraulogic/game/GameLogic.kt
+++ app/src/main/java/com/arnyminerz/paraulogic/game/GameLogic.kt
@@ -1,2 +1,11 @@
 package com.arnyminerz.paraulogic.game
 
+import com.arnyminerz.paraulogic.storage.entity.IntroducedWord
+
+fun List<IntroducedWord>.getTutis(gameInfo: GameInfo): List<IntroducedWord> {
+    val tutis = arrayListOf<IntroducedWord>()
+    for (word in this)
+        if (gameInfo.isTuti(word.word))
+            tutis.add(word)
+    return tutis
+}
