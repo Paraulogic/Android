581c21ee146563a71eb92d9bcb633098bf757e2b
Arnau Mora <arnyminer.z@gmail.com>
2022-03-08 21:11:35 +0100

    :bug: Fixed `Context.activity`
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- app/src/main/java/com/arnyminerz/paraulogic/utils/ComposeUtils.kt
+++ app/src/main/java/com/arnyminerz/paraulogic/utils/ComposeUtils.kt
@@ -1,7 +1,9 @@
 package com.arnyminerz.paraulogic.utils
 
+import android.app.Activity
 import android.content.Context
 import android.content.ContextWrapper
+import androidx.activity.ComponentActivity
 import androidx.appcompat.app.AppCompatActivity
 
 /**
@@ -9,9 +11,10 @@ import androidx.appcompat.app.AppCompatActivity
  * @author Arnau Mora
  * @since 20220308
  */
-val Context.activity: AppCompatActivity?
+val Context.activity: Activity?
     get() = when (this) {
         is AppCompatActivity -> this
+        is ComponentActivity -> this
         is ContextWrapper -> baseContext.activity
         else -> null
     }
