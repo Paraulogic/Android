52ef5af362f098a21d4fa6f137db232df29c307d
Arnau Mora <arnyminer.z@gmail.com>
2022-03-09 00:41:04 +0100

    :wrench: Opted in for `ExperimentalPagerApi`
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- app/src/main/java/com/arnyminerz/paraulogic/activity/MainActivity.kt
+++ app/src/main/java/com/arnyminerz/paraulogic/activity/MainActivity.kt
@@ -8,9 +8,10 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import com.arnyminerz.paraulogic.ui.elements.MainScreen
 import com.arnyminerz.paraulogic.ui.theme.AppTheme
 import com.arnyminerz.paraulogic.ui.viewmodel.MainViewModel
+import com.google.accompanist.pager.ExperimentalPagerApi
 
 class MainActivity : ComponentActivity() {
-    @OptIn(ExperimentalMaterial3Api::class)
+    @OptIn(ExperimentalMaterial3Api::class, ExperimentalPagerApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
