2070df817b44eb8be09a8a4ce469d129498683ff
Arnau Mora <arnyminer.z@gmail.com>
2022-03-08 21:10:04 +0100

    :sparkles: Added error reporting disabling
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- app/src/main/java/com/arnyminerz/paraulogic/App.kt
+++ app/src/main/java/com/arnyminerz/paraulogic/App.kt
@@ -1,13 +1,19 @@
 package com.arnyminerz.paraulogic
 
 import android.app.Application
+import com.arnyminerz.paraulogic.pref.PreferencesModule
+import com.arnyminerz.paraulogic.pref.dataStore
 import io.sentry.SentryLevel
 import io.sentry.android.core.SentryAndroid
 import io.sentry.android.timber.SentryTimberIntegration
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.runBlocking
 import timber.log.Timber
 import timber.log.Timber.Forest.plant
 
 class App : Application() {
+
     override fun onCreate() {
         super.onCreate()
 
@@ -21,6 +27,13 @@ class App : Application() {
                 )
             else
                 plant(Timber.DebugTree())
+            val errorReportingEnabled = runBlocking {
+                dataStore
+                    .data
+                    .map { preferences -> preferences[PreferencesModule.ErrorReportingEnabledKey] }
+                    .first()
+            }
+            options.enableAllAutoBreadcrumbs(errorReportingEnabled ?: true)
         }
     }
 }
