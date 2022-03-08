2a5e4ea62c013b24e55e99b65a6d7bfc1ba77d4d
Arnau Mora <arnyminer.z@gmail.com>
2022-03-08 16:19:25 +0100

    :sparkles: Sketched games sign in
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- app/src/main/java/com/arnyminerz/paraulogic/MainActivity.kt
+++ app/src/main/java/com/arnyminerz/paraulogic/MainActivity.kt
@@ -3,14 +3,19 @@ package com.arnyminerz.paraulogic
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Person
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
@@ -19,16 +24,38 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.arnyminerz.paraulogic.play.games.createSignInClient
+import com.arnyminerz.paraulogic.play.games.signInSilently
+import com.arnyminerz.paraulogic.play.games.startSignInIntent
 import com.arnyminerz.paraulogic.ui.Game
 import com.arnyminerz.paraulogic.ui.theme.AppTheme
 import com.arnyminerz.paraulogic.ui.viewmodel.MainViewModel
+import com.arnyminerz.paraulogic.utils.doAsync
 import com.arnyminerz.paraulogic.utils.launchUrl
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import timber.log.Timber
 
 class MainActivity : ComponentActivity() {
+    internal val signInResultLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            val data = result.data ?: return@registerForActivityResult
+            val signInResult = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
+            if (signInResult?.isSuccess == true) {
+                Timber.i("Signed in correctly.")
+                val signedInAccount = signInResult.signInAccount
+            } else {
+                Timber.e("Sign in status: ${signInResult?.status}")
+            }
+        }
+
+    private lateinit var signInClient: GoogleSignInClient
+
     @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        signInClient = createSignInClient()
 
         setContent {
             val viewModel by viewModels<MainViewModel>()
@@ -57,7 +84,12 @@ class MainActivity : ComponentActivity() {
                                     modifier = Modifier
                                         .fillMaxWidth(.6f)
                                 )
-                            }
+                            },
+                            actions = {
+                                IconButton(onClick = { startSignInIntent(signInClient) }) {
+                                    Icon(Icons.Rounded.Person, contentDescription = "Login button")
+                                }
+                            },
                         )
                     }
                 ) { paddingValues ->
@@ -72,6 +104,15 @@ class MainActivity : ComponentActivity() {
                     }
                 }
             }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        doAsync {
+            val account = signInSilently(signInClient)
+            if (account == null)
+                Timber.e("User must sign in manually.")
         }
     }
 }
