9474d9e960816e920ec96c3e148edcdae14e5f50
Arnau Mora <arnyminer.z@gmail.com>
2022-03-08 16:18:40 +0100

    :sparkles: Added `Authentication.kt`
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

---
+++ app/src/main/java/com/arnyminerz/paraulogic/play/games/Authentication.kt
@@ -0,0 +1,47 @@
+package com.arnyminerz.paraulogic.play.games
+
+import com.arnyminerz.paraulogic.MainActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import timber.log.Timber
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
+
+fun MainActivity.startSignInIntent(client: GoogleSignInClient) {
+    val intent = client.signInIntent
+    signInResultLauncher.launch(intent)
+}
+
+fun MainActivity.createSignInClient(): GoogleSignInClient =
+    GoogleSignIn.getClient(this, GoogleSignInOptions.DEFAULT_GAMES_SIGN_IN)
+/*GoogleSignIn.getClient(
+    this,
+    GoogleSignInOptions
+        .Builder(GoogleSignInOptions.DEFAULT_GAMES_SIGN_IN)
+        .requestScopes(Games.SCOPE_GAMES)
+        .build()
+)*/
+
+suspend fun MainActivity.signInSilently(client: GoogleSignInClient) =
+    suspendCoroutine<GoogleSignInAccount?> { cont ->
+        Timber.d("Checking if signed in...")
+        val signInOptions = GoogleSignInOptions.DEFAULT_GAMES_SIGN_IN
+        val account = GoogleSignIn.getLastSignedInAccount(this)
+        if (GoogleSignIn.hasPermissions(account, *signInOptions.scopeArray))
+            cont.resume(account!!)
+        else {
+            Timber.d("Logging in with Games...")
+            client
+                .silentSignIn()
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful)
+                        cont.resume(task.result)
+                    else {
+                        Timber.e("User needs to sign in manually.")
+                        cont.resume(null)
+                    }
+                }
+        }
+    }
