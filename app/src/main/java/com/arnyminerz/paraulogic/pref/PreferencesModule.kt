4904725d9fb62d87ac0cefd292bb676fbd5d6c56
Arnau Mora <arnyminer.z@gmail.com>
2022-03-08 21:07:43 +0100

    :sparkles: Added `PreferencesModule.kt`
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

---
+++ app/src/main/java/com/arnyminerz/paraulogic/pref/PreferencesModule.kt
@@ -0,0 +1,11 @@
+package com.arnyminerz.paraulogic.pref
+
+import android.content.Context
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+
+val Context.dataStore by preferencesDataStore(name = "Paraulogic")
+
+object PreferencesModule {
+    val ErrorReportingEnabledKey = booleanPreferencesKey(PREF_ERROR_REPORTING)
+}
