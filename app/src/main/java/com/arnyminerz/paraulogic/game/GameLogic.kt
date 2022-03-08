138e32862a316c7078b8f61ff7463b6443e02b76
Arnau Mora <arnyminer.z@gmail.com>
2022-03-08 17:13:50 +0100

    :sparkles: Added `calculatePoints`
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- app/src/main/java/com/arnyminerz/paraulogic/game/GameLogic.kt
+++ app/src/main/java/com/arnyminerz/paraulogic/game/GameLogic.kt
@@ -9,3 +9,17 @@ fun List<IntroducedWord>.getTutis(gameInfo: GameInfo): List<IntroducedWord> {
             tutis.add(word)
     return tutis
 }
+
+fun List<IntroducedWord>.calculatePoints(gameInfo: GameInfo): Int {
+    var points = 0
+    for (iWord in this) {
+        val len = iWord.word.length
+        points += when {
+            len == 3 -> 1
+            len == 4 -> 2
+            gameInfo.isTuti(iWord.word) -> 10
+            else -> iWord.word.length
+        }
+    }
+    return points
+}
