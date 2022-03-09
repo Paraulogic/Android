f1bd0764bfdec33d65bb4f0968dab88a4309cba0
Arnau Mora <arnyminer.z@gmail.com>
2022-03-09 14:16:01 +0100

    :sparkles: Added `uiContext`
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- app/src/main/java/com/arnyminerz/paraulogic/utils/AsyncUtils.kt
+++ app/src/main/java/com/arnyminerz/paraulogic/utils/AsyncUtils.kt
@@ -1,10 +1,21 @@
 package com.arnyminerz.paraulogic.utils
 
+import androidx.annotation.UiThread
 import androidx.annotation.WorkerThread
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 fun doAsync(@WorkerThread call: suspend CoroutineScope.() -> Unit) {
     CoroutineScope(Dispatchers.IO).launch { call(this) }
+}
+
+/**
+ * Runs [call] on the UI thread.
+ * @author Arnau Mora
+ * @since 20220309
+ */
+suspend fun uiContext(@UiThread call: suspend CoroutineScope.() -> Unit) {
+    withContext(Dispatchers.Main) { call(this) }
 }
