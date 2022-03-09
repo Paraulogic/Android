44b09c0657f3429979836f92afb6b64ac6c2ec0c
Arnau Mora <arnyminer.z@gmail.com>
2022-03-09 02:13:00 +0100

    :art: Put day summary into a card
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- app/src/main/java/com/arnyminerz/paraulogic/ui/screen/StatsScreen.kt
+++ app/src/main/java/com/arnyminerz/paraulogic/ui/screen/StatsScreen.kt
@@ -2,8 +2,12 @@ package com.arnyminerz.paraulogic.ui.screen
 
 import android.text.format.DateFormat
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -11,7 +15,9 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.arnyminerz.paraulogic.R
 import com.arnyminerz.paraulogic.game.GameHistoryItem
 import com.arnyminerz.paraulogic.game.maxDate
@@ -22,6 +28,7 @@ import timber.log.Timber
 import java.util.Calendar
 
 @Composable
+@ExperimentalMaterial3Api
 fun StatsScreen(viewModel: MainViewModel, gameHistory: SnapshotStateList<GameHistoryItem>) {
     val selectDateText = stringResource(R.string.action_select_date)
     var buttonText by remember { mutableStateOf(selectDateText) }
@@ -82,39 +89,46 @@ fun StatsScreen(viewModel: MainViewModel, gameHistory: SnapshotStateList<GameHis
     }
     if (historyItem != null && dayFoundWords.isNotEmpty())
         Row {
-            Text(
-                text = if (historyItemTutis.size == 1)
-                    if (dayFoundTutis.size == historyItemTutis.size)
+            Card(
+                modifier = Modifier
+                    .padding(start = 16.dp, end = 16.dp)
+                    .fillMaxWidth()
+            ) {
+                Text(
+                    modifier = Modifier.padding(8.dp),
+                    text = if (historyItemTutis.size == 1)
+                        if (dayFoundTutis.size == historyItemTutis.size)
+                            stringResource(
+                                R.string.stats_day_tuti,
+                                buttonText,
+                                dayFoundWords.size,
+                                historyItemWords.size
+                            )
+                        else
+                            stringResource(
+                                R.string.stats_day_no_tuti,
+                                buttonText,
+                                dayFoundWords.size,
+                                historyItemWords.size
+                            )
+                    else if (dayFoundTutis.size == historyItemTutis.size)
                         stringResource(
-                            R.string.stats_day_tuti,
+                            R.string.stats_day_all_tutis,
                             buttonText,
                             dayFoundWords.size,
-                            historyItemWords.size
+                            historyItemWords.size,
+                            historyItemTutis.size
                         )
                     else
                         stringResource(
-                            R.string.stats_day_no_tuti,
+                            R.string.stats_day_n_tutis,
                             buttonText,
                             dayFoundWords.size,
-                            historyItemWords.size
+                            historyItemWords.size,
+                            dayFoundTutis.size,
+                            historyItemTutis.size
                         )
-                else if (dayFoundTutis.size == historyItemTutis.size)
-                    stringResource(
-                        R.string.stats_day_all_tutis,
-                        buttonText,
-                        dayFoundWords.size,
-                        historyItemWords.size,
-                        historyItemTutis.size
-                    )
-                else
-                    stringResource(
-                        R.string.stats_day_n_tutis,
-                        buttonText,
-                        dayFoundWords.size,
-                        historyItemWords.size,
-                        dayFoundTutis.size,
-                        historyItemTutis.size
-                    )
-            )
+                )
+            }
         }
 }
