fc077338635fb77512e8464a9a972393d409e0a8
Arnau Mora <arnyminer.z@gmail.com>
2022-03-12 11:19:32 +0100

    :sparkles: Added `serverData`

--- app/src/main/java/com/arnyminerz/paraulogic/game/Loader.kt
+++ app/src/main/java/com/arnyminerz/paraulogic/game/Loader.kt
@@ -1,7 +1,9 @@
 package com.arnyminerz.paraulogic.game
 
 import android.content.Context
+import com.google.firebase.FirebaseException
 import com.google.firebase.firestore.Query
+import com.google.firebase.firestore.QuerySnapshot
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.ktx.initialize
@@ -12,15 +14,21 @@ import kotlin.coroutines.resumeWithException
 import kotlin.coroutines.suspendCoroutine
 
 /**
- * Obtains the last game info from the Firestore database.
+ * Fetches the server's data.
  * @author Arnau Mora
  * @since 20220312
- * @param context The context to initialize the Firestore instance from.
- * @throws NoSuchElementException Could not get the game data from server.
+ * @param context The context to initialize Firebase Firestore from.
+ * @param limit How many items to load at most.
+ * @param sortDirection The sort direction according to timestamp.
+ * @throws FirebaseException If there happens to be an error while loading data from server.
  */
-@AddTrace(name = "DataLoad")
-@Throws(NoSuchElementException::class)
-suspend fun loadGameInfoFromServer(context: Context) = suspendCoroutine<GameInfo> { cont ->
+@AddTrace(name = "ServerLoad")
+@Throws(FirebaseException::class)
+private suspend fun serverData(
+    context: Context,
+    limit: Long = 1,
+    sortDirection: Query.Direction = Query.Direction.DESCENDING
+) = suspendCoroutine<QuerySnapshot> { cont ->
     try {
         Firebase.firestore
     } catch (e: IllegalStateException) {
@@ -29,20 +37,32 @@ suspend fun loadGameInfoFromServer(context: Context) = suspendCoroutine<GameInfo
         Firebase.firestore
     }
         .collection("paraulogic")
-        .orderBy("timestamp", Query.Direction.DESCENDING)
-        .limit(1)
+        .orderBy("timestamp", sortDirection)
+        .limit(limit)
         .get()
-        .addOnSuccessListener { snapshot ->
-            if (snapshot.isEmpty)
-                cont.resumeWithException(NoSuchElementException("Could not get data from server"))
-            else {
-                Timber.d("Got documents, fetching first...")
-                val documents = snapshot.documents
-                val document = documents.first()
-                Timber.d("Decoding GameInfo...")
-                val gameInfo = GameInfo.fromServer(document)
-                cont.resume(gameInfo)
-            }
-        }
+        .addOnSuccessListener { cont.resume(it) }
         .addOnFailureListener { cont.resumeWithException(it) }
+}
+
+/**
+ * Obtains the last game info from the Firestore database.
+ * @author Arnau Mora
+ * @since 20220312
+ * @param context The context to initialize the Firestore instance from.
+ * @throws FirebaseException If there happens to be an error while loading data from server.
+ * @throws NoSuchElementException Could not get the game data from server.
+ */
+@AddTrace(name = "DataLoad")
+@Throws(NoSuchElementException::class, FirebaseException::class)
+suspend fun loadGameInfoFromServer(context: Context): GameInfo {
+    val snapshot = serverData(context)
+    if (snapshot.isEmpty)
+        throw NoSuchElementException("Could not get data from server")
+    else {
+        Timber.d("Got documents, fetching first...")
+        val documents = snapshot.documents
+        val document = documents.first()
+        Timber.d("Decoding GameInfo...")
+        return GameInfo.fromServer(document)
+    }
 }
