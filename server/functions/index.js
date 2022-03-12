dfb13e007419ebf631ce90f1785b348ddb463d15
Arnau Mora <arnyminer.z@gmail.com>
2022-03-12 10:55:27 +0100

    :sparkles: Added `fetchSource` endpoint

--- server/functions/index.js
+++ server/functions/index.js
@@ -4,16 +4,29 @@ const admin = require("firebase-admin");
 
 const {fetchSource, decodeSource} = require('./loader');
 
+const makeDataRequest = async () => {
+    console.info("Downloading data from Paraulògic...");
+    const source = await fetchSource();
+    console.info("Parsing data...")
+    return decodeSource(source);
+};
+
+exports.fetchSource = functions.https
+    .onRequest(async (request, response) => {
+        if(!process.env.FUNCTIONS_EMULATOR)
+            return response.send({"error":"not-dev-environment"});
+
+        const gameInfo = await makeDataRequest();
+        response.send(gameInfo);
+    });
+
 exports.scheduledFunctionCrontab = functions.pubsub.schedule('0 5 * * *')
     .timeZone('Europe/Madrid')
     .onRun(async () => {
         admin.initializeApp();
         const firestore = admin.firestore();
 
-        console.info("Downloading data from Paraulògic...");
-        const source = await fetchSource();
-        console.info("Parsing data...")
-        const gameInfo = decodeSource(source);
+        const gameInfo = makeDataRequest();
 
         console.info("Creating collection...")
         const collection = firestore.collection("paraulogic");
