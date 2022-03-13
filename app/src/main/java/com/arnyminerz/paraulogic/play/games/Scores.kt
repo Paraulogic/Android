870d63f53c1b4393e4497ee68ff43f09f896422d
Arnau Mora <arnyminer.z@gmail.com>
2022-03-13 15:35:51 +0100

    :broom: Cleaned up code

--- app/src/main/java/com/arnyminerz/paraulogic/play/games/Scores.kt
+++ app/src/main/java/com/arnyminerz/paraulogic/play/games/Scores.kt
@@ -1,12 +1,18 @@
 package com.arnyminerz.paraulogic.play.games
 
 import android.content.Context
+import androidx.annotation.WorkerThread
 import com.arnyminerz.paraulogic.R
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.games.AnnotatedData
 import com.google.android.gms.games.Games
+import com.google.android.gms.games.LeaderboardsClient
 import com.google.android.gms.games.leaderboard.LeaderboardVariant
 import timber.log.Timber
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlin.coroutines.suspendCoroutine
 
 private var missingAddedPoints = arrayListOf<Long>()
 
@@ -15,7 +21,8 @@ private var missingAddedPoints = arrayListOf<Long>()
  * @author Arnau Mora
  * @since 20220309
  */
-fun tryToAddPoints(context: Context) {
+@WorkerThread
+suspend fun tryToAddPoints(context: Context) {
     val points = missingAddedPoints.sum()
     if (points > 0)
         addPlayerPoints(
@@ -29,12 +36,28 @@ fun tryToAddPoints(context: Context) {
         )
 }
 
+@WorkerThread
+private suspend fun LeaderboardsClient.getLeaderboardAnnotatedData(
+    leaderboardId: String,
+    maxResults: Int = 1
+) = suspendCoroutine<AnnotatedData<LeaderboardsClient.LeaderboardScores>> { cont ->
+    loadPlayerCenteredScores(
+        leaderboardId,
+        LeaderboardVariant.TIME_SPAN_ALL_TIME,
+        LeaderboardVariant.COLLECTION_PUBLIC,
+        maxResults,
+    )
+        .addOnSuccessListener { cont.resume(it) }
+        .addOnFailureListener { cont.resumeWithException(it) }
+}
+
 /**
  * Adds [points] to the player's leaderboard.
  * @author Arnau Mora
  * @since 20220309
  */
-private fun addPlayerPoints(
+@WorkerThread
+private suspend fun addPlayerPoints(
     context: Context,
     points: Long,
     loginRequired: () -> Unit,
@@ -46,56 +69,47 @@ private fun addPlayerPoints(
         Timber.d("Getting leaderboard id...")
         val leaderboardId = context.getString(R.string.leaderboard_world_ranking)
 
-        Timber.v("Getting current player leaderboard...")
         val leaderboardsClient = Games.getLeaderboardsClient(context, account)
-        val limitResultsTo = 1
-        leaderboardsClient
-            .loadPlayerCenteredScores(
-                leaderboardId,
-                LeaderboardVariant.TIME_SPAN_ALL_TIME,
-                LeaderboardVariant.COLLECTION_PUBLIC,
-                limitResultsTo,
-            )
-            .addOnSuccessListener { leaderboardScoreAnnotatedData ->
-                if (leaderboardScoreAnnotatedData != null) {
-                    val leaderboardScore = leaderboardScoreAnnotatedData.get()
-                    if (leaderboardScore != null)
-                        try {
-                            val scores = leaderboardScore.scores
-                            Timber.d("Scores (${scores.count}): $scores")
 
-                            val score = if (scores.count <= 0)
-                                0
-                            else
-                                scores
-                                    .get(0)
-                                    .rawScore
-                            Timber.d("Current player score: $score")
-
-                            val newScore = score + points
-                            Timber.v("Submitting new score: $newScore")
-                            leaderboardsClient.submitScore(leaderboardId, newScore)
-                            onAdded()
-                        } catch (e: IllegalStateException) {
-                            Timber.e(e, "Could not get score.")
-                        } finally {
-                            leaderboardScore.release()
-                        }
-                    else
-                        Timber.e("Could not fetch user score. Leaderboard score is null.")
-                } else
-                    Timber.e("Could not fetch user score. Annotated data is null.")
+        try {
+            Timber.v("Getting current player leaderboard...")
+            val lsad = leaderboardsClient.getLeaderboardAnnotatedData(leaderboardId, 1)
+            val leaderboardScore = lsad.get() ?: run {
+                Timber.e("Could not fetch user score. Leaderboard score is null.")
+                return
             }
-            .addOnFailureListener { error ->
-                Timber.e(error, "Could not load leaderboard.")
-                (error as? ApiException)?.let { e ->
-                    if (e.statusCode == 26502) { // CLIENT_RECONNECT_REQUIRED
-                        missingAddedPoints.add(points)
-                        loginRequired()
-                    }
-                    errorListener(e)
+
+            try {
+                val scores = leaderboardScore.scores
+                Timber.d("Scores (${scores.count}): $scores")
+
+                val score = if (scores.count <= 0)
+                    0
+                else
+                    scores
+                        .get(0)
+                        .rawScore
+                Timber.d("Current player score: $score")
+
+                val newScore = score + points
+                Timber.v("Submitting new score: $newScore")
+                leaderboardsClient.submitScore(leaderboardId, newScore)
+                onAdded()
+            } catch (e: IllegalStateException) {
+                Timber.e(e, "Could not get score.")
+            } finally {
+                leaderboardScore.release()
+            }
+        } catch (error: Exception) {
+            Timber.e(error, "Could not load leaderboard.")
+            (error as? ApiException)?.let { e ->
+                if (e.statusCode == 26502) { // CLIENT_RECONNECT_REQUIRED
+                    missingAddedPoints.add(points)
+                    loginRequired()
                 }
+                errorListener(e)
             }
+        }
     }
 }
 
@@ -104,5 +118,5 @@ private fun addPlayerPoints(
  * @author Arnau Mora
  * @since 20220309
  */
-fun addPlayerPoints(context: Context, points: Long, loginRequired: () -> Unit) =
+suspend fun addPlayerPoints(context: Context, points: Long, loginRequired: () -> Unit) =
     addPlayerPoints(context, points, loginRequired, { }, { })
