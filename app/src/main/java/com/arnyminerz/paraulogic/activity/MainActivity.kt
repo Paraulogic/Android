181b56aa0344aba3b676ffd632a3ffeb9c0dc179
Arnau Mora <arnyminer.z@gmail.com>
2022-04-28 22:54:59 +0100

    :sparkles: Added donation dialog
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- app/src/main/java/com/arnyminerz/paraulogic/activity/MainActivity.kt
+++ app/src/main/java/com/arnyminerz/paraulogic/activity/MainActivity.kt
@@ -4,27 +4,26 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.*
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.res.stringResource
+import androidx.datastore.preferences.core.edit
 import androidx.lifecycle.ViewModelProvider
 import com.arnyminerz.paraulogic.R
 import com.arnyminerz.paraulogic.activity.model.LanguageActivity
-import com.arnyminerz.paraulogic.play.games.createSignInClient
-import com.arnyminerz.paraulogic.play.games.loadSnapshot
-import com.arnyminerz.paraulogic.play.games.signInSilently
-import com.arnyminerz.paraulogic.play.games.startSignInIntent
-import com.arnyminerz.paraulogic.play.games.tryToAddPoints
+import com.arnyminerz.paraulogic.play.games.*
+import com.arnyminerz.paraulogic.pref.PreferencesModule
+import com.arnyminerz.paraulogic.pref.dataStore
 import com.arnyminerz.paraulogic.singleton.DatabaseSingleton
 import com.arnyminerz.paraulogic.storage.entity.IntroducedWord
 import com.arnyminerz.paraulogic.ui.elements.MainScreen
 import com.arnyminerz.paraulogic.ui.theme.AppTheme
 import com.arnyminerz.paraulogic.ui.toast
 import com.arnyminerz.paraulogic.ui.viewmodel.MainViewModel
-import com.arnyminerz.paraulogic.utils.doAsync
-import com.arnyminerz.paraulogic.utils.doOnUi
-import com.arnyminerz.paraulogic.utils.mapJsonObject
-import com.arnyminerz.paraulogic.utils.toJsonArray
+import com.arnyminerz.paraulogic.utils.*
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.android.gms.auth.api.Auth
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
@@ -117,6 +116,52 @@ class MainActivity : LanguageActivity() {
             AppTheme {
                 MainScreen(snackbarHostState, viewModel, popupLauncher) {
                     startSignInIntent(signInClient, signInLauncher)
+                }
+
+                var showingDialog by remember { mutableStateOf(false) }
+                if (showingDialog)
+                    AlertDialog(
+                        onDismissRequest = { showingDialog = false },
+                        title = {
+                            Text(
+                                text = stringResource(R.string.dialog_coffee_title),
+                            )
+                        },
+                        text = {
+                            Text(
+                                text = stringResource(R.string.dialog_coffee_message),
+                            )
+                        },
+                        confirmButton = {
+                            TextButton(
+                                onClick = { launchUrl("https://ko-fi.com/arnyminerz") },
+                            ) {
+                                Text(
+                                    text = stringResource(R.string.action_buy_coffee),
+                                )
+                            }
+                        },
+                        dismissButton = {
+                            TextButton(
+                                onClick = {
+                                    doAsync {
+                                        dataStore.edit {
+                                            it[PreferencesModule.ShownDonateDialog] = true
+                                        }
+                                    }
+                                    showingDialog = false
+                                },
+                            ) {
+                                Text(
+                                    text = stringResource(R.string.action_not_show_again),
+                                )
+                            }
+                        },
+                    )
+
+                doAsync {
+                    if (dataStore.data.first()[PreferencesModule.ShownDonateDialog] != true)
+                        uiContext { showingDialog = true }
                 }
             }
 
