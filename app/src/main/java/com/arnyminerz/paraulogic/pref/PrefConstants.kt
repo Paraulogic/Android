f22976e993e8bc6835f1b570a966a2b35045df84
Arnau Mora <(arnyminer.z@gmail.com)>
2022-05-04 11:13:35 +0200

    :sparkles: Added analytics consent toggle
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- app/src/main/java/com/arnyminerz/paraulogic/pref/PrefConstants.kt
+++ app/src/main/java/com/arnyminerz/paraulogic/pref/PrefConstants.kt
@@ -2,6 +2,8 @@ package com.arnyminerz.paraulogic.pref
 
 const val PREF_ERROR_REPORTING = "errorReporting"
 
+const val PREF_ANALYTICS = "enableAnalytics"
+
 const val PREF_LANGUAGE = "language"
 
 const val PREF_SHOWN_LOGIN = "shownLogin"
