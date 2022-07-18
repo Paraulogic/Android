d9c686892efa5d88f394b53a954e25b2b6cfcac8
Arnau Mora <arnyminer.z@gmail.com>
2022-07-18 17:09:22 +0200

    :truck: Migrated to Android 13
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- app/src/main/java/com/arnyminerz/paraulogic/activity/FeedbackActivity.kt
+++ app/src/main/java/com/arnyminerz/paraulogic/activity/FeedbackActivity.kt
@@ -2,12 +2,12 @@ package com.arnyminerz.paraulogic.activity
 
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material3.ExperimentalMaterial3Api
-import com.arnyminerz.paraulogic.activity.model.LanguageActivity
 import com.arnyminerz.paraulogic.ui.screen.FeedbackScreen
 import com.arnyminerz.paraulogic.ui.theme.AppTheme
 
-class FeedbackActivity : LanguageActivity() {
+class FeedbackActivity : AppCompatActivity() {
     @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
