626d19e7e660c46e3974a92ad4ce3263bb0d19d5
Arnau Mora <arnyminer.z@gmail.com>
2022-07-28 10:25:46 +0200

    :fire: Removed Worker scheduling
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- app/src/main/java/com/arnyminerz/paraulogic/App.kt
+++ app/src/main/java/com/arnyminerz/paraulogic/App.kt
@@ -1,15 +1,9 @@
 package com.arnyminerz.paraulogic
 
 import android.app.Application
-import androidx.work.Constraints
-import androidx.work.NetworkType
-import androidx.work.PeriodicWorkRequestBuilder
-import androidx.work.WorkManager
 import com.arnyminerz.paraulogic.log.CrashReportingTree
-import com.arnyminerz.paraulogic.worker.DataFetchWorker
 import timber.log.Timber
 import timber.log.Timber.Forest.plant
-import java.util.concurrent.TimeUnit
 
 class App : Application() {
     override fun onCreate() {
@@ -20,14 +14,6 @@ class App : Application() {
         else
             plant(CrashReportingTree())
 
-        Timber.i("Programming data fetcher...")
-        PeriodicWorkRequestBuilder<DataFetchWorker>(24, TimeUnit.HOURS)
-            .setConstraints(
-                Constraints.Builder()
-                    .setRequiredNetworkType(NetworkType.CONNECTED)
-                    .build()
-            )
-            .build()
-            .let { WorkManager.getInstance(this).enqueue(it) }
+
     }
 }
