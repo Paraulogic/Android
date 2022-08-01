2f9d73936b8ec2727a6e9a93c746cbea00c647cc
Arnau Mora <Arnyminer.Z@gmail.com>
2022-08-01 10:40:10 +0200

    🐛 Fixed `weight` import (#110)

--- app/src/main/java/com/arnyminerz/paraulogic/ui/Game.kt
+++ app/src/main/java/com/arnyminerz/paraulogic/ui/Game.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
