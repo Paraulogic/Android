076590a7da205283ebff806dbfe45917ad45265b
Arnau Mora <arnyminer.z@gmail.com>
2022-08-17 10:42:47 +0200

    :fire: Removed old messaging code
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- app/src/main/java/com/arnyminerz/paraulogic/App.kt
+++ app/src/main/java/com/arnyminerz/paraulogic/App.kt
@@ -2,8 +2,6 @@ package com.arnyminerz.paraulogic
 
 import android.app.Application
 import com.arnyminerz.paraulogic.log.CrashReportingTree
-import com.google.firebase.ktx.Firebase
-import com.google.firebase.messaging.ktx.messaging
 import timber.log.Timber
 import timber.log.Timber.Forest.plant
 
@@ -15,12 +13,5 @@ class App : Application() {
             plant(Timber.DebugTree())
         else
             plant(CrashReportingTree())
-
-        Timber.i("Subscribing to \"gameInfo\" messaging topic.")
-        Firebase.messaging.subscribeToTopic("gameInfo")
-            .addOnSuccessListener {
-                Timber.i("Subscribed to \"gameInfo\".")
-            }
-            .addOnFailureListener { Timber.e(it, "Could not subscribe to \"gameInfo\".") }
     }
 }
