2bdbf5357f058d7fd0c2049ff9e73c9991979ea8
Arnau Mora <(arnyminer.z@gmail.com)>
2022-08-17 12:38:50 +0200

    :fire: Removed `RESULT_FIREBASE_EXCEPTION`
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- app/src/main/java/com/arnyminerz/paraulogic/annotation/LoadError.kt
+++ app/src/main/java/com/arnyminerz/paraulogic/annotation/LoadError.kt
@@ -1,12 +1,11 @@
 package com.arnyminerz.paraulogic.annotation
 
 import androidx.annotation.IntDef
-import com.arnyminerz.paraulogic.annotation.LoadError.Companion.RESULT_FIREBASE_EXCEPTION
 import com.arnyminerz.paraulogic.annotation.LoadError.Companion.RESULT_NO_SUCH_ELEMENT
 import com.arnyminerz.paraulogic.annotation.LoadError.Companion.RESULT_OK
 
 @Target(AnnotationTarget.PROPERTY, AnnotationTarget.TYPE)
-@IntDef(RESULT_OK, RESULT_NO_SUCH_ELEMENT, RESULT_FIREBASE_EXCEPTION)
+@IntDef(RESULT_OK, RESULT_NO_SUCH_ELEMENT)
 annotation class LoadError {
     companion object {
         /**
@@ -23,12 +22,5 @@ annotation class LoadError {
          * data.
          */
         const val RESULT_NO_SUCH_ELEMENT = 1
-
-        /**
-         * @author Arnau Mora
-         * @since 20220428
-         * An error occurred while loading data from Firebase.
-         */
-        const val RESULT_FIREBASE_EXCEPTION = 2
     }
 }
