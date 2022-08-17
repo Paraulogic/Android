51c365dd21e630da62012c4494ce965b7f004988
Arnau Mora <(arnyminer.z@gmail.com)>
2022-08-17 12:39:09 +0200

    :sparkles: Added `UpdateGameDataReceiver`
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- app/src/main/java/com/arnyminerz/paraulogic/App.kt
+++ app/src/main/java/com/arnyminerz/paraulogic/App.kt
@@ -1,11 +1,16 @@
 package com.arnyminerz.paraulogic
 
 import android.app.Application
+import android.content.IntentFilter
+import com.arnyminerz.paraulogic.broadcast.ACTION_UPDATE_CLOCK
+import com.arnyminerz.paraulogic.broadcast.UpdateGameDataReceiver
 import com.arnyminerz.paraulogic.log.CrashReportingTree
 import timber.log.Timber
 import timber.log.Timber.Forest.plant
 
 class App : Application() {
+    private val br = UpdateGameDataReceiver()
+
     override fun onCreate() {
         super.onCreate()
 
@@ -13,5 +18,8 @@ class App : Application() {
             plant(Timber.DebugTree())
         else
             plant(CrashReportingTree())
+
+        val filter = IntentFilter(ACTION_UPDATE_CLOCK)
+        registerReceiver(br, filter)
     }
 }
