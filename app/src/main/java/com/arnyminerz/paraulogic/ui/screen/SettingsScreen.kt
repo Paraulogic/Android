026b75f770ac082d99e3f1aa10dfe18a1cb94b96
Arnau Mora <Arnyminer.Z@gmail.com>
2022-08-19 01:30:20 +0200

    ✨ Added version to the bottom of settings (#121)
    * :globe_with_meridians: Added `settings_footer`
    
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>
    
    * :sparkles: Added version footer
    
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>
    
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>
    Co-authored-by: Arnau Mora <(arnyminer.z@gmail.com)>

--- app/src/main/java/com/arnyminerz/paraulogic/ui/screen/SettingsScreen.kt
+++ app/src/main/java/com/arnyminerz/paraulogic/ui/screen/SettingsScreen.kt
@@ -1,6 +1,8 @@
 package com.arnyminerz.paraulogic.ui.screen
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.ExperimentalMaterialApi
@@ -10,6 +12,7 @@ import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
@@ -22,6 +25,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.arnyminerz.paraulogic.BuildConfig
 import com.arnyminerz.paraulogic.R
 import com.arnyminerz.paraulogic.activity.DonationsActivity
 import com.arnyminerz.paraulogic.activity.FeedbackActivity
@@ -60,83 +65,104 @@ fun SettingsScreen() {
                 colors = TopAppBarDefaults.centerAlignedTopAppBarColors()
             )
         }
-    ) {
+    ) { paddingValues ->
         Column(
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(it)
+                .fillMaxSize()
+                .padding(paddingValues),
         ) {
-            var enableErrorReporting by rememberBooleanPreference(
-                PREF_ERROR_REPORTING,
-                initialValue = true,
-                defaultValue = true
-            )
-            var enableAnalytics by rememberBooleanPreference(
-                PREF_ANALYTICS,
-                initialValue = true,
-                defaultValue = true,
-            )
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f)
+            ) {
+                var enableErrorReporting by rememberBooleanPreference(
+                    PREF_ERROR_REPORTING,
+                    initialValue = true,
+                    defaultValue = true
+                )
+                var enableAnalytics by rememberBooleanPreference(
+                    PREF_ANALYTICS,
+                    initialValue = true,
+                    defaultValue = true,
+                )
 
-            val analytics = Firebase.analytics
-            val crashlytics = Firebase.crashlytics
+                val analytics = Firebase.analytics
+                val crashlytics = Firebase.crashlytics
 
-            var showLanguageDialog by remember { mutableStateOf(false) }
-            if (showLanguageDialog)
-                LanguageDialog { showLanguageDialog = false }
+                var showLanguageDialog by remember { mutableStateOf(false) }
+                if (showLanguageDialog)
+                    LanguageDialog { showLanguageDialog = false }
 
-            SettingsCategory(text = stringResource(R.string.settings_category_general))
-            SettingsItem(
-                title = stringResource(R.string.settings_general_language_title),
-                subtitle = stringResource(R.string.settings_general_language_summary),
-                onClick = { showLanguageDialog = true }
-            )
+                SettingsCategory(text = stringResource(R.string.settings_category_general))
+                SettingsItem(
+                    title = stringResource(R.string.settings_general_language_title),
+                    subtitle = stringResource(R.string.settings_general_language_summary),
+                    onClick = { showLanguageDialog = true }
+                )
 
-            SettingsCategory(text = stringResource(R.string.settings_category_info))
-            SettingsItem(
-                title = stringResource(R.string.settings_info_github_title),
-                subtitle = stringResource(R.string.settings_info_github_summary),
-                onClick = { context.launchUrl("https://github.com/ArnyminerZ/Paraulogic-Android") }
-            )
-            SettingsItem(
-                title = stringResource(R.string.settings_info_feedback_title),
-                subtitle = stringResource(R.string.settings_info_feedback_summary),
-                onClick = { context.launch(FeedbackActivity::class.java) }
-            )
-            SettingsItem(
-                title = stringResource(R.string.settings_info_donation_title),
-                subtitle = stringResource(R.string.settings_info_donation_summary),
-                onClick = { context.launch(DonationsActivity::class.java) }
-            )
+                SettingsCategory(text = stringResource(R.string.settings_category_info))
+                SettingsItem(
+                    title = stringResource(R.string.settings_info_github_title),
+                    subtitle = stringResource(R.string.settings_info_github_summary),
+                    onClick = { context.launchUrl("https://github.com/ArnyminerZ/Paraulogic-Android") }
+                )
+                SettingsItem(
+                    title = stringResource(R.string.settings_info_feedback_title),
+                    subtitle = stringResource(R.string.settings_info_feedback_summary),
+                    onClick = { context.launch(FeedbackActivity::class.java) }
+                )
+                SettingsItem(
+                    title = stringResource(R.string.settings_info_donation_title),
+                    subtitle = stringResource(R.string.settings_info_donation_summary),
+                    onClick = { context.launch(DonationsActivity::class.java) }
+                )
 
-            SettingsCategory(text = stringResource(R.string.settings_category_advanced))
-            SettingsItem(
-                title = stringResource(R.string.settings_error_reporting_title),
-                subtitle = stringResource(R.string.settings_error_reporting_summary),
-                stateBoolean = enableErrorReporting,
-                setBoolean = { state ->
-                    enableErrorReporting = state
-                    crashlytics.setCrashlyticsCollectionEnabled(enableErrorReporting)
-                },
-                onClick = {
-                    enableErrorReporting = !enableErrorReporting
-                    crashlytics.setCrashlyticsCollectionEnabled(enableErrorReporting)
-                },
-                switch = true,
-            )
-            SettingsItem(
-                title = stringResource(R.string.settings_analytics_title),
-                subtitle = stringResource(R.string.settings_analytics_summary),
-                stateBoolean = enableAnalytics,
-                setBoolean = { state ->
-                    enableAnalytics = state
-                    analytics.setAnalyticsCollectionEnabled(enableAnalytics)
-                },
-                onClick = {
-                    enableAnalytics = !enableAnalytics
-                    analytics.setAnalyticsCollectionEnabled(enableAnalytics)
-                },
-                switch = true,
-            )
+                SettingsCategory(text = stringResource(R.string.settings_category_advanced))
+                SettingsItem(
+                    title = stringResource(R.string.settings_error_reporting_title),
+                    subtitle = stringResource(R.string.settings_error_reporting_summary),
+                    stateBoolean = enableErrorReporting,
+                    setBoolean = { state ->
+                        enableErrorReporting = state
+                        crashlytics.setCrashlyticsCollectionEnabled(enableErrorReporting)
+                    },
+                    onClick = {
+                        enableErrorReporting = !enableErrorReporting
+                        crashlytics.setCrashlyticsCollectionEnabled(enableErrorReporting)
+                    },
+                    switch = true,
+                )
+                SettingsItem(
+                    title = stringResource(R.string.settings_analytics_title),
+                    subtitle = stringResource(R.string.settings_analytics_summary),
+                    stateBoolean = enableAnalytics,
+                    setBoolean = { state ->
+                        enableAnalytics = state
+                        analytics.setAnalyticsCollectionEnabled(enableAnalytics)
+                    },
+                    onClick = {
+                        enableAnalytics = !enableAnalytics
+                        analytics.setAnalyticsCollectionEnabled(enableAnalytics)
+                    },
+                    switch = true,
+                )
+            }
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+            ) {
+                Text(
+                    stringResource(
+                        R.string.settings_footer,
+                        BuildConfig.VERSION_NAME,
+                        BuildConfig.VERSION_CODE
+                    ),
+                    style = MaterialTheme.typography.labelMedium,
+                    modifier = Modifier
+                        .padding(8.dp),
+                )
+            }
         }
     }
 }
